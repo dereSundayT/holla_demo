2af61b0e3e34ac95713d7f71122068a2feeda2d6
DereSundayT <toluwanisundaydere@gmail.com>
2020-04-20 23:56:21 +0100

    final commit

--- index.php
+++ index.php
@@ -1,33 +1,41 @@
 <?php
-$bill_url = 'billing.com/amount?=';
+//$bill_url = "https://www.google.com.ng/";
+$bill_url ="https://swapi.dev/api/films/";
+
+
 //connect to db
     $con = mysqli_connect("localhost","root","","demo");
     if (mysqli_connect_errno()){
         echo "Failed to connect to MySQL: " . mysqli_connect_error();
         die();
     }
-    //selecting data from the table user
-    $result = mysqli_query($con,"SELECT * FROM `billings`");
+    //selecting data from the table user that are yet to be billed
+    $result = mysqli_query($con,"SELECT * FROM `billings` where `status`=0");
     $userDetails = array();
     while($row_user = mysqli_fetch_assoc($result)){
         $userDetails[] = $row_user;
     }
-    //echo '<pre>'; print_r( json_decode(json_encode($userDetails)));
-    //die();
-    //$userDetails =  json_decode(json_encode($userDetails));
-    $user_req = array_chunk($userDetails,6,true);
+    $user_req = array_chunk($userDetails,37,true);
     foreach($user_req as $req){
         foreach($req as $sub_req_keys => $sub_req_val){
-            $url = $bill_url.$sub_req_val['amount_to_bill'];
+           $url = $bill_url.$sub_req_val['id'];
             try{
                 $response = file_get_contents($url);
                 if($response !==false){
-                    echo $response;
+                    $data = json_decode(json_encode($response));
+                    echo "<pre>"; print_r($data);
                     //update db
+                    $id = $sub_req_val['id'];
+                    $update =mysqli_query($con,"UPDATE billings SET status='1' WHERE id=$id");
                 }
             }catch(Exception $e){
-                echo $e->getMessage();
+                //echo $e->getMessage();
+                //update db
+                $id = $sub_req_keys['id'];
+                $update =mysqli_query($con,"UPDATE billings SET status='0' WHERE id=$id");
             }
+           
         }
+        echo "<hr/> Request:" .$sub_req_keys;
     }
   
--- readme.md
+++ readme.md
@@ -1,1 +1,24 @@
-git
+ So. 
+
+In the php code. Do less looping.
+.
+
+So if I am to read the payment information in another table for each user I will write a single SQL command to fetch the user'e data from the users table and the payment information (bank related information)
+
+This way I am doing the fetching of all the data I will need in my code at once. And looping through all at once
+
+At once or in small chunks (remember)
+
+ Also while talking to the third party API. I simply send charge request, I won't wait for the response. So I can move to the other (hmmm is this possible in PHP?
+
+So basically what I'm trying to say here is that
+
+I will store a reference to the current user I am on. Send a charge, wait for the API to respond but I will have a limited timeout
+
+A small amount of timeout in case of network connection or the third party API is taking time to do their stuffs
+
+If all goes well and they respond on time I will mark the user billed and go to the next user
+ Or I will leave the user unbilled (take note of this) and move to the next year, the chances that the user will be billed is high
+It's just that I couldn't wait to listen to the responses
+Then when all the users are billed
+I do a check to know if the unbilled user has bene billed (verifying from the third party API using the reference I stored
